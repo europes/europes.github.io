11821f049adbcf90f03297f2c0adf115936c4015
europes <64372763+europes@users.noreply.github.com>
2021-01-27 02:48:07 +0000

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -14,4 +14,17 @@ menuLinks.forEach(
   function(menuLink) { 
     menuLink.addEventListener("click", toggleHamburger) 
   }
-)
+
+
+Var menu = document.getElementById('menu');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+        menu.classList.add("nav-colored");
+        menu.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
