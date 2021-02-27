e1880ca32ec1a79e5a2f459984f4d7c5b973b513
europes <64372763+europes@users.noreply.github.com>
2021-02-27 18:36:51 +0000

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -16,27 +16,6 @@ menuLinks.forEach(
   }
 )
 
-var hams = document.getElementsByClassName('ham');
-window.onscroll = function () {     
-    if (document.body.scrollTop >= 200 ) {   
-      hams.classList.add("fixed");
-        hams.classList.remove("relative");       
-    } 
-    else {
-      hams.classList.add("relative");
-        hams.classList.remove("fixed");
-    }
-};
 
 
-var close = document.getElementsByClassName('showClose');
-window.onscroll = function () {     
-    if (document.body.scrollTop >= 200 ) {   
-      close.classList.add("fixed");
-        close.classList.remove("relative");       
-    } 
-    else {
-      close.classList.add("realtive");
-        close.classList.remove("fixed");
-    }
-};
+
