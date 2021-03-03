23871b0db952e268437dbdb5f043185a41b830f4
europes <64372763+europes@users.noreply.github.com>
2021-03-03 19:26:38 +0000

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,11 +1,10 @@
-document.write('<div class="footer-container">
+document.write("<div class="footer-container">
 		<div class="prefoot"></div>
 <div class="footer">
 		<br><br><br><br>
 <p1>&copy; 2021 JMZ Web Designs </p1>
 </div>
-</div>
-');
+</div>");
 
 
 
